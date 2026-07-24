83eb5315c08a74f104bcbb9b80aba41199a95a81
aarjavdevx <aarjavj018@gmail.com>
2026-07-24 21:27:25 +0530

    commit 2

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,4 +1,4 @@
-import { signupUser } from "../Services/authService.js"
+import { loginUser, signupUser } from "../Services/authService.js"
 
 export const signup = async(req, res)=>{
     try {
@@ -6,10 +6,27 @@ export const signup = async(req, res)=>{
         res.status(201).json({
             success: true,
             message: "User created successfully",
-            ...user
+            user
         })
     } catch (error) {
         console.log("Error while signing up",error)
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const login = async(req, res)=>{
+    try {
+        const userData = await loginUser(req.body)
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            userData
+        })
+    } catch (error) {
+        console.log("Error while logging in",error)
         res.status(400).json({
             success: false,
             message: error.message
--- Services/authService.js
+++ Services/authService.js
@@ -1,6 +1,6 @@
 import { createUser, findUserByEmail } from "../Repositories/userRepository.js"
 import bcrypt from "bcrypt"
-
+import jwt from 'jsonwebtoken'
 
 export const signupUser = async({name, email, password, dateofbirth, gender, bio, college})=>{
     const existingUser = await findUserByEmail(email)
@@ -11,5 +11,22 @@ export const signupUser = async({name, email, password, dateofbirth, gender, bio
     const user = await createUser({name, email, bio, college, dateofbirth, gender, password: hashedPassword})
     return {userId: user._id, name: user.name, email: user.email, bio: user.bio,
         college: user.college, dateofbirth: user.dateofbirth, gender: user.gender}
+}
+
+export const loginUser = async({email, password}) =>{
+    const user = await findUserByEmail(email)
+    if(!user){
+        throw new Error("Invalid email or password")
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+        throw new Error("Invalid email or password")
+    }
+    const token = await jwt.sign({userId: user._id},process.env.JWT_SECRET, {expiresIn: '24h'})
+    return {token, user:{
+        userId: user._id, userEmail: user.email, 
+        userName: user.name, userBio: user.bio, 
+        userCollege: user.college, userDateofbirth: user.dateofbirth, 
+        userGender: user.gender} }
 }
 
