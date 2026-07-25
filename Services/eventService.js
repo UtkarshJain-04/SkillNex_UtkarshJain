8de0a857a87c82a6b3cb59f1d4945ee37ad53e2c
aarjavdevx <aarjavj018@gmail.com>
2026-07-25 18:29:58 +0530

    Added event feed

--- Services/eventService.js
+++ Services/eventService.js
@@ -1,4 +1,5 @@
-import { createEvent, findEventById, findEventByIdAndUpdate } from "../Repositories/eventRepository.js"
+import { createEvent, findEventById, findEventByIdAndDelete, findEventByIdAndUpdate, getAllEvents, getMyEvents } from "../Repositories/eventRepository.js"
+import { findUserById } from "../Repositories/userRepository.js"
 
 export const createNewEvent = async({title, description, category, mode, eligibility, startDate, endDate, regDeadline, status, organizer, prize, venue})=>{
     const event = await createEvent({title, description, category, mode, eligibility, startDate, endDate, regDeadline, status, organizer, prize, venue})
@@ -15,4 +16,31 @@ export const updateEventById = async(eventId, updates)=>{
     }
     const updatedEvent = await findEventByIdAndUpdate(eventId, updates)
     return updatedEvent
+}
+
+export const deleteEventById = async(eventId)=>{
+    const event = await findEventById(eventId)
+    if(!event){
+        throw new Error("Event doesn't exists")
+    }
+    return await findEventByIdAndDelete(eventId)
+}
+
+export const getEventFeed = async(userId)=>{
+    const user = await findUserById(userId)
+    if(!user){
+        throw new Error("User doesn't exists")
+    }
+    const allEvents = await getAllEvents()
+    const myFeed =  allEvents.filter((event)=>{
+        return event.organizer.toString()!==userId.toString()
+    })
+    return myFeed
+}
+
+export const fetchMyEventsById = async(userId)=>{
+    if(!getMyEvents(userId)){
+        throw new Error("You haven't organised events yet")
+    }
+    return await getMyEvents(userId)
 }
