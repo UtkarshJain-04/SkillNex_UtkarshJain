c6531f453d4c56f7967ff8953715d26e1e7433b6
UtkarshJain-04 <utkarshjainuj04@gmail.com>
2026-07-30 09:06:22 +0530

    Update eventAiRoutes.js

--- Routes/eventAiRoutes.js
+++ Routes/eventAiRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import protect from '../middlewares/authMiddleware.js';
+import protect from '../Middlewares/authMiddleware.js';
 import { analyzeEvents } from '../Controllers/eventAiController.js';
 
 const router = express.Router()
